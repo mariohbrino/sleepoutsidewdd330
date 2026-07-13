cd0fd6f3122a659e202c62dd6d6c345edbb6f446
brinley-precious <imukprecious@gmail.com>
2026-07-13 15:13:59 +0100

    Add remove from cart feature

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,28 +1,51 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  attachRemoveListeners();
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${import.meta.env.BASE_URL}${item.Image.substring(1)}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  return `
+    <li class="cart-card divider">
+      <button class="remove-item" data-id="${item.Id}">✕</button>
 
-  return newItem;
+      <a href="#" class="cart-card__image">
+        <img
+          src="${import.meta.env.BASE_URL}${item.Image.substring(1)}"
+          alt="${item.Name}"
+        />
+      </a>
+
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+    </li>
+  `;
+}
+renderCartContents();
+
+function removeFromCart(id) {
+  let cartItems = getLocalStorage("so-cart");
+
+  cartItems = cartItems.filter(item => item.Id !== id);
+
+  setLocalStorage("so-cart", cartItems);
+
+  renderCartContents();
+  attachRemoveListeners();
 }
 
-renderCartContents();
+function attachRemoveListeners() {
+  document.querySelectorAll(".remove-item").forEach((button) => {
+    button.addEventListener("click", () => {
+      removeFromCart(button.dataset.id);
+    });
+  });
+}
